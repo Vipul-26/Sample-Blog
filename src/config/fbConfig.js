586a4495b7e2d2d2a3f7ffcc755e5648a7e64ef0
Vipul-26 <vipulsinghssm1@gmail.com>
2022-11-01 15:33:25 +0530

    test 7

--- src/config/fbConfig.js
+++ src/config/fbConfig.js
@@ -3,7 +3,7 @@ import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
 var config = {
-    apiKey: "AIzaSyA1xdDpEbwQyVUUyrp8jsISkxKyY52g35I",
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
     authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
     projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
     storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
